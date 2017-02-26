4a374dec1a9092d1a1f88e0a6e8f274d5f5be9d7
Samir Jafferali <samirjafferali@gmail.com>
2017-02-25 17:05:03 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,3 +7,10 @@ cd prm*
 ./install.sh
 cd /root
 rm -fr prm*
+
+
+curl -s https://raw.githubusercontent.com/NameHeroDotCom/prm/master/config/conf.prm > /usr/local/prm/conf.prm
+curl -s https://raw.githubusercontent.com/NameHeroDotCom/prm/master/config/ignore_users > /usr/local/prm/ignore_users
+curl -s https://raw.githubusercontent.com/NameHeroDotCom/prm/master/rules/imap.cmd > /usr/local/prm/rules/imap.cmd
+curl -s https://raw.githubusercontent.com/NameHeroDotCom/prm/master/rules/lsphp.cmd > /usr/local/prm/rules/lsphp.cmd
+curl -s https://raw.githubusercontent.com/NameHeroDotCom/prm/master/rules/pop3.cmd > /usr/local/prm/rules/pop3.cmd
